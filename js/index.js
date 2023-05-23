7049232b855b23470e5ad68679b1923dfabdb0e1
bluedec <yisussaez@gmail.com>
2023-05-22 22:41:57 -0300

    Hello world

--- js/index.js
+++ js/index.js
@@ -59,62 +59,62 @@ function hide_newsletter() {
 let is_nav_open = false;
 
 function show_side_nav_bar() {
-	if (is_nav_open) {
-		close_side_nav_bar();
-		return
-	}
-	const nav = document.getElementById("side-nav-bar");
-	const dimmer = document.getElementById("screen-dimmer");
+    if (is_nav_open) {
+        close_side_nav_bar();
+        return
+    }
+    const nav = document.getElementById("side-nav-bar");
+    const dimmer = document.getElementById("screen-dimmer");
 
-	dimmer.style.zIndex = 20;
-	is_nav_open = true;
-	
-	let right_position = -260;
-	let dimmer_opacity = 0;
-	let sum = 11;
+    dimmer.style.zIndex = 20;
+    is_nav_open = true;
 
-	const interv = setInterval(function() {
-		dimmer_opacity += 0.01;
-		right_position += sum;
-		sum -= 0.2;
+    let right_position = -260;
+    let dimmer_opacity = 0;
+    let sum = 11;
 
-		dimmer.style.opacity = dimmer_opacity;
-		if (right_position < 0) {
-			nav.style.right = `${right_position}px` 
-		}
+    const interv = setInterval(function() {
+        dimmer_opacity += 0.01;
+        right_position += sum;
+        sum -= 0.2;
 
-		if (dimmer_opacity > 0.49 && right_position > -5) {
-			clearInterval(interv);
-		}
-	}, 5);
+        dimmer.style.opacity = dimmer_opacity;
+        if (right_position < 0) {
+            nav.style.right = `${right_position}px`
+        }
+
+        if (dimmer_opacity > 0.49 && right_position > -5) {
+            clearInterval(interv);
+        }
+    }, 5);
 
 }
 
 function close_side_nav_bar() {
-	if (is_nav_open === false) {
-		return;
-	}
-	const nav = document.getElementById("side-nav-bar");
-	const dimmer = document.getElementById("screen-dimmer");
+    if (is_nav_open === false) {
+        return;
+    }
+    const nav = document.getElementById("side-nav-bar");
+    const dimmer = document.getElementById("screen-dimmer");
 
-	is_nav_open = false;
-	
-	let right_position = 0;
-	let dimmer_opacity = 0.5;
+    is_nav_open = false;
 
-	const interv = setInterval(function() {
-		dimmer_opacity -= 0.01;
-		right_position -= 7;
+    let right_position = 0;
+    let dimmer_opacity = 0.5;
 
-		dimmer.style.opacity = dimmer_opacity;
-		nav.style.right = `${right_position}px` 
-		
-		if (dimmer_opacity < 0.01 && right_position < -260) {
-			clearInterval(interv);
-			dimmer.style.zIndex = -20;
-		}
-	}, 5);
-	
+    const interv = setInterval(function() {
+        dimmer_opacity -= 0.01;
+        right_position -= 7;
+
+        dimmer.style.opacity = dimmer_opacity;
+        nav.style.right = `${right_position}px`
+
+        if (dimmer_opacity < 0.01 && right_position < -260) {
+            clearInterval(interv);
+            dimmer.style.zIndex = -20;
+        }
+    }, 5);
+
 }
 
 // function that is passed an html element and changes it's 
@@ -131,5 +131,3 @@ function change_opacity_with_className(classN, op) {
 		elements[i].style.opacity = op;
 	}
 }
-
-
